8a02fc6a2bbcd207dac91059cadd97f363380ab3
ghanshyam-du <ghanshyamd816@gmail.com>
2026-05-02 18:43:01 +0530

    added login logic

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,33 @@
+import User from "../model/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "user not found" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({ message: "invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.json({ token, role: user.role, name: user.name, id: user._id });
+        
+        return res.status(200).json({ message: "login successful", token });
+    }
+    catch (error) {
+        return res.status(500).json({ message: "internal server error" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
 connectDB()
 .then(() => {
     console.log("Database connected successfully");
