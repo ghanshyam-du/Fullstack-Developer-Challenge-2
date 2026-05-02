180b24ded587924f715605945cf24bdfea547633
ghanshyam-du <ghanshyamd816@gmail.com>
2026-05-02 10:46:48 +0530

    database is connected successfully with project used monogodb and added the user model.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/connection.js';
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+connectDB()
+.then(() => {
+    console.log("Database connected successfully");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
 });
