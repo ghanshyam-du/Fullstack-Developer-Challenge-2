7cb35457dd9779473f38d79cfc2d7c09133382e7
ghanshyam-du <ghanshyamd816@gmail.com>
2026-05-03 14:03:32 +0530

    added the logic of feedbaack like feedbacksubmition and getallfeedback for admin view

---
+++ backend/controller/feedback.controller.js
@@ -0,0 +1,49 @@
+import Feedback from "../model/feedback.model";
+import Assignment from "../model/assignment.model";
+
+const feedbackSubmit = async(req, res) =>{
+    try{
+        const {comments} = req.body;
+        const {assignmentId} = req.params;
+
+        const assignment = await Assignment.findById(assignmentId);
+        if(!assignment){
+            return res.status(404).json({message: "Assignment not found"});
+        }
+
+        if(assignment.reviewer.toString() !== req.user.userId){
+            return res.status(403).json({message: "You are not permitted to submit feedback for this assignment"});
+        }
+        
+        if(assignment.status === "submitted"){
+            return res.status(400).json({message: "Feedback for this assignment has already been submitted"});
+        }
+
+        const feedback = await Feedback.create({
+            comments,
+            assignment: assignmentId,
+            createdBy: req.user.userId
+        });
+        assignment.status = "submitted";
+        await assignment.save();
+        res.status(201).json({message: "Feedback submitted successfully", feedback});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+
+const getAllFeedbacks = async(req, res) =>{
+    try{
+        const feedback = await Feedback.find().populate("assignment", "review reviewee reviewer").populate("createdBy", "name email");
+        res.json(feedback);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export default { feedbackSubmit, getAllFeedbacks };
