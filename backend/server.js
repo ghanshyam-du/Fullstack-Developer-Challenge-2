2c57590b3a41622abd7fbdff67cea7f80921cfbe
ghanshyam-du <ghanshyamd816@gmail.com>
2026-05-03 14:11:25 +0530

    added auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/connection.js';
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/api/auth", authRoutes);
 
 
 connectDB()
